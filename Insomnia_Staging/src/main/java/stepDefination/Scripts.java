b717c0d97bb7bbad8552ae4b11a8306e3f3adb17
Sandytrp <Sandytrp@gmail.com>
2017-10-05 16:02:06 +0530

    1st successful commit

--- Insomnia_Staging/src/main/java/stepDefination/Scripts.java
+++ Insomnia_Staging/src/main/java/stepDefination/Scripts.java
@@ -1,15 +1,17 @@
 package stepDefination;
 
 import org.openqa.selenium.WebDriver;
-import cucumber.api.java.en.Given;
+/*import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import pom.EventPage;
+import pom.EventPage;*/
 
 public class Scripts extends AbstractWebDriver {
-	WebDriver driver = getDriver();
-
-	@Given("^Open Website$")
+//	WebDriver driver = getDriver();
+public static void main(String[] args) {
+	System.out.println("Hello Jenkins");
+}
+	/*@Given("^Open Website$")
 	public void i_opened_Website() {
 		driver.navigate().to("https://insomniacookies.com/");
 	}
@@ -24,12 +26,12 @@ public class Scripts extends AbstractWebDriver {
 
 	@Then("^I validate title and URL$")
 	public void i_print_title_and_URL() {
-		/*
+		
 		 * assertEquals(
 		 * "Selenium Framework | Selenium, Cucumber, Ruby, Java et al."
 		 * ,driver.getTitle());
 		 * assertEquals("http://www.seleniumframework.com/",driver.getCurrentUrl
 		 * ());
-		 */
-	}
+		 
+	}*/
 }
--- Insomnia_Staging/src/main/java/testRunner/RunCukesTest.java
+++ Insomnia_Staging/src/main/java/testRunner/RunCukesTest.java
@@ -1,12 +1,12 @@
 package testRunner;
 
-import org.junit.runner.RunWith;
+/*import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
+import cucumber.api.junit.Cucumber;*/
 
-@RunWith(Cucumber.class)
-@CucumberOptions(features = "C:\\Users\\Adminindia\\workspace\\Insomnia_Staging\\Feature\\feature.feature", glue = "stepDefination", tags ={"@Tag"} )
+/*@RunWith(Cucumber.class)
+@CucumberOptions(features = "C:\\Users\\Adminindia\\workspace\\Insomnia_Staging\\Feature\\feature.feature", glue = "stepDefination", tags ={"@Tag"} )*/
 	
 public class RunCukesTest {
 }	
